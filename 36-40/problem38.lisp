d4f11fd3f2d375ea2aecab12a8ecf8782d1a297c
moratori <hoko1993@yahoo.co.jp>
2013-11-11 12:47:46 +0900

    update

--- 36-40/problem38.lisp
+++ 36-40/problem38.lisp
@@ -4,5 +4,26 @@
 
 
 (defun pandigital? (n)
-  
-  )
+  (set-equal? (getdigit n) '(1 2 3 4 5 6 7 8 9)))
+
+
+(defun getcan (n)
+  (labels 
+	((main (acc result)
+	   (if (> (length result)  8)
+		 result
+		 (main (1+ acc) (append result (getdigit (* n acc))))))) (main 1 nil)))
+
+
+(print 
+  (apply #'max
+	(remove-if-not #'pandigital?
+(mapcar #'digit->num
+		(remove-if-not 
+  			(lambda (x) (= (length x) 9))
+  				(print (mapcar #'getcan (range1-n 9999)))))
+	))) 
+
+
+
+
