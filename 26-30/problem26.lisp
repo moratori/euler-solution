18f08e68c2793bcf4348d0e2d70536bd5f705b8f
moratori <hoko1993@yahoo.co.jp>
2013-11-12 11:05:17 +0900

    update

--- 26-30/problem26.lisp
+++ 26-30/problem26.lisp
@@ -1,24 +1,37 @@
 
+
+
+#|
+	
+
+	 1 / 210 = 0...1
+    10 / 210 = 0...10
+   100 / 210 = 0...100
+  1000 / 210 = 4...160
+  1600 / 210 = 7...130
+  1300 / 210 = 6...40
+   400 / 210 = 1...190
+  1900 / 210 = 9...10
+ 
+      *                 *
+  (1 10 100 160 130 40 190 10)
+
+  おんなじあまりが出てきたらあきらかにそのあとは循環
+
+|#
+
+
 (use :euler.util.my)
 
+(defun cyclen (d) 
+  (labels 
+	((main (acc lst)
+	  (let* ((r (mod acc d)) 
+			 (i (position r lst)))
+		(if (not (null i))
+		  (- (length lst) i)
+		  (main (* r 10)  (append1 lst r)))))) (main 1 nil)))
 
-;; (0 1 4 2 8 5 7  1 4 2 8 5 7  1 4 2 8 5 7)
-;; (0 2 2 7 8  2 2 7 8  2 2 7 8  2 2 7 8)
-(defun circling? (lst)
-  nil
-  )
 
-;; (getdecimal 1/7) -> ( 1 4 2 8 5 7 ~)
-;; (getdecimal 1/5) -> ( 2 -)
-(defun getdecimal (ratn &optional (result nil))
-  (sleep 1)
-  (format t "result = ~A~%" result)
-  (multiple-value-bind (upper lower) (floor ratn)
-	(cond
-	  ((circling? result))
-	  ((zerop lower) 
-	   (reverse (cons '- (cons upper result))))
-	  (t 
-		(getdecimal (* lower 10) (cons upper result))))))
+(print (maximize #'cyclen 1 999))
 
-(print (getdecimal  ))
