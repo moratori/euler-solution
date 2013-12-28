d95319c5a6f7efd1e53f7d77f3d4b542276d4356
moratori <hoko1993@yahoo.co.jp>
2013-12-28 22:07:58 +0900

    brute force...

--- 41-45/problem43.lisp
+++ 41-45/problem43.lisp
@@ -1,81 +1,44 @@
 
 (use :euler.util.my)
 
+;;;; --dynamic-space-size : 2000
 
-;; satisfies purely condition
+
+;;; satisfies purely condition
 (defun such? (n)
   (and (pandigital? n :start 0)
 	   (destructuring-bind 
 		 (d1 d2 d3 d4 d5 d6 d7 d8 d9 d10) (getdigit n)
 		 (declare (ignore d1))
 		 (and 
-		   (div? (digit->num (list d2 d3 d4)) 2)
-		   (div? (digit->num (list d3 d4 d5)) 3)
-		   (div? (digit->num (list d4 d5 d6)) 5)
-		   (div? (digit->num (list d5 d6 d7)) 7)
-		   (div? (digit->num (list d6 d7 d8)) 11)
-		   (div? (digit->num (list d7 d8 d9)) 13)
+		   (div? (digit->num (list d2 d3 d4))  2)
+		   (div? (digit->num (list d3 d4 d5))  3)
+		   (div? (digit->num (list d4 d5 d6))  5)
+		   (div? (digit->num (list d5 d6 d7))  7)
+		   (div? (digit->num (list d6 d7 d8))  11)
+		   (div? (digit->num (list d7 d8 d9))  13)
 		   (div? (digit->num (list d8 d9 d10)) 17)))))
 
-;; 条件を満たす 3桁の数を返す
-(defun sat-num (prime)
-  (remove-if-not 
-	(lambda (x) (and (div? x prime) (duplicate-not? (getdigit x))))
-	(range1-n 999 100)))
+;;; '(0 1 2 3 4 5 6 7 8 9)
+;;;  10 9 8 7 6 5 4 3 2 1
+;;; 9! = 362880
+;;;
+;;; (1 2 3)
+;;; 1 2 3
+;;; 1 3 2
+;;; 2 1 3
+;;; 2 3 1
+;;; 3 1 2
+;;; 3 2 1
 
-
-;; l1で使える数
-(defun useble (l1 l2)
-  (remove-if-not 
-  	(lambda (x)
-		(member-if 
-	  		(lambda (y)
-				(equal (cdr (getdigit x)) (cdr (getdigit y)))) 
-			l2)) l1))
-
-(defun check (a b c d e f g)
-  (let ((sa (getdigit a))
-		)
-	t)
+(print 
+  (loop for x in (subseq (perm '(0 1 2 3 4 5 6 7 8 9)) (fact 9))
+	  for each = (digit->num x)
+	  if (such? each)
+	  sum each)
   )
 
 
-(setq 
-  c3 
-  (intersection 
-	(useble (sat-num 3) (sat-num 2)) 
-	(useble (sat-num 3) (sat-num 5)))
-  c2
-  (useble (sat-num 2) c3)
-  c5
-  (intersection 
-	(useble (sat-num 5) c3)
-	(useble (sat-num 5) (sat-num 7)))
-  c7
-  (intersection
-	(useble (sat-num 7) c5)
-	(useble (sat-num 7) (sat-num 11)))
-  c11
-  (intersection
-	(useble (sat-num 11) c7)
-	(useble (sat-num 11) (sat-num 13)))
-  c13
-  (intersection
-	(useble (sat-num 13) c11)
-	(useble (sat-num 13) (sat-num 17)))
-  c17
-  	(useble (sat-num 17) c13))
-
-#|
-[(list a b c d e f g) 
- 	| a <- (sat-num 2)
-	  b <- (sat-num 3)
-	  c <- (sat-num 5)
-	  d <- (sat-num 7)
-	  e <- (sat-num 11)
-	  f <- (sat-num 13)
-	  g <- (sat-num 17) (check a b c d e f g)]
-|#
 
 
-(print c5)
+
