2439a0936cfde187501ed52c5aea916120c3dde6
moratori <hoko1993@yahoo.co.jp>
2014-01-17 14:07:36 +0900

    いい〜

--- 206-210/problem206.lisp
+++ 206-210/problem206.lisp
@@ -1,31 +1,28 @@
 
+
 (use :euler.util.my)
 
-
-(defun join (templ tar)
-  (append1 
-	(flatten (zip templ tar)) 
-	(lastelm templ)))
-
-(defun make (n)
-  (let1 split (getdigit n)
-	(digit->num
-	  (join 
-	  '(1 2 3 4 5 6 7 8 9 0)
-	  (append 
-	  	(make-list (- 9 (length split)) :initial-element 0) 
-	  	split)))))
-
+(defun check? (x)
+  (destructuring-bind (a b c d e f g h i j k l m n o p q r s) (getdigit x)
+	(declare (ignore b d f h j l n p r))
+	(and (= a 1)
+		 (= c 2)
+		 (= e 3)
+		 (= g 4)
+		 (= i 5)
+		 (= k 6)
+		 (= m 7)
+		 (= o 8)
+		 (= q 9)
+		 (= s 0))))
 
 (print 
-  (sqrt 
-	(loop named exit 
-		for x from 0
-		upto 999999999
-		do
-		(let1 num (make x)
-			  (when (square? num)
-				(print num)
-				(return-from exit num))))))
+  (time (loop 
+			named exit
+			for x 
+			from (floor (sqrt 1020304050607080900)) 
+			upto (floor (sqrt 1929394959697989990))
+			do (when (check? (* x x))
+				 (return-from exit x)))))
 
 
